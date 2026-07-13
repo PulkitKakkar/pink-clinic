eceb1029dc39829b02a584650495987a17b36d99
Pulkit Kakkar <pulkitkakkarrdec@gmail.com>
2026-07-13 15:53:01 +0100

    fix: configure embedded Studio base path

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,6 +8,7 @@ import { structure } from "@/sanity/structure";
 export default defineConfig({
   name: "pink-beauty",
   title: "Pink Beauty Content Studio",
+  basePath: "/studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "replace-me",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   plugins: [structureTool({ structure })],
